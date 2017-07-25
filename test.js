4151e92a78e6ca8c305d6e50929db3a9542df18a
Shannon <reidy.pf@gmail.com>
2017-07-25 12:11:52 -0500

    project finished

---
+++ test.js
@@ -0,0 +1,57 @@
+// let yourName = "Shannon";
+// let instructorName = "Devon";
+// let statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+// console.log(statement);
+// let high = 50;
+// let low  = 10;
+// let math = high - low;
+// A: 40
+
+
+// 6.
+// What is `math` set to?
+// math = high - "5";
+// console.log(math);
+
+let x = (false == 0);
+// A. true
+console.log(x);
+
+// 12.
+let y = (false == "");
+// A. true
+console.log(y);
+
+// 13.
+let z = (0 == "");
+// A. true
+console.log(z);
+
+// 14.
+let a = (null == null);
+// A. true
+console.log(a);
+
+// 25.
+let b = (undefined == undefined);
+// A. true
+console.log(b);
+
+// 16.
+let c = (undefined == null);
+// A.
+console.log(c);
+
+// 17.
+let d = (null == false);
+// A.
+console.log(d);
+
+// 18.
+let e = (NaN == null);
+// A.
+console.log(e);
+
+
+// 19.
+let f = (NaN == NaN);
